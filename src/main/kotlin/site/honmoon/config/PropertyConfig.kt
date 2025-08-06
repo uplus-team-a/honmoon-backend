15edeb9d0054a782e13a5aeba2f181a0d145a4e2
jiwondev <jiwondev@i-nara.co.kr>
2025-08-06 12:56:41 +0900

    build: 서버 배포 dev 추가

--- src/main/kotlin/site/honmoon/config/PropertyConfig.kt
+++ src/main/kotlin/site/honmoon/config/PropertyConfig.kt
@@ -1,9 +1,11 @@
 package site.honmoon.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.context.annotation.PropertySource
 import org.springframework.context.annotation.PropertySources
 
+@Profile("local")
 @Configuration
 @PropertySources(
     PropertySource("classpath:properties/env.properties")
